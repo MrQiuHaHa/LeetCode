81dbf5789706114e8716bb688f18fd001c2ef209
qiujr <>
2021-03-17 12:50:39 +0800

    完善快速排序

--- 912. 排序数组-冒泡选择归并快排/912. 排序数组-冒泡选择归并快排/main.cpp
+++ 912. 排序数组-冒泡选择归并快排/912. 排序数组-冒泡选择归并快排/main.cpp
@@ -75,7 +75,7 @@ void selectionSort(vector<int>& nums) {
 }
 
 // 归并排序
-class Solution {
+class MergeSort {
     vector<int> leftArr;
     vector<int> numsArr;
 public:
@@ -93,8 +93,8 @@ public:
         }
         
         int mid = (begin + end) >> 1;
-        mergeSort(begin, mid);
-        mergeSort(mid, end);
+        mergeSort(begin, mid); // [begin, mid)
+        mergeSort(mid, end);   // [mid, end)
         // 此处begin到mid以及mid到end下标应该都是有序了。且begin-mid-end在原数组中就是相邻的元素
         //接下来合并两个有序数组
         merge(begin, mid, end);
@@ -120,6 +120,68 @@ public:
     }
 };
 
+// 快速排序
+class QuickSort {
+    
+    vector<int> numsArr;
+public:
+    
+    vector<int> sortArray(vector<int>& nums) {
+        numsArr = nums;
+        quickSort(0, (int)nums.size());
+        return numsArr;
+    }
+    // [begin, end) 左闭右开
+    void quickSort(int begin, int end) {
+        if (end - begin < 2) { // 至少要有两个元素才可以排序
+            return;
+        }
+        
+        int mid = pivotIndex(begin, end); // 已确定的轴点不再需要参与后面的任何事情
+        quickSort(begin, mid); //[begin, mid)
+        quickSort(mid+1, end); //[mid+1, end) 这里必须+1，其实是（除去mid轴点这个元素）后的两边重新递归构造轴点
+    }
+private:
+    // 核心方法：构造出begin-end间的轴点元素的位置，
+    // 备份begin位置的元素为轴点元素，小于轴点的放begin左边，大于轴点的放begin右边，全部完成最终返回轴点的位置
+    int pivotIndex(int begin, int end) {
+        int pivot = numsArr[begin];//备份出轴点元素
+        end--; // 先减1指向最后一个元素，[begin, end) 右边是开区间的设计
+        
+        // 左右循环扫描。（原地将数组以某个元素为中间切分成两边）
+        while (begin < end) {
+            
+            while (begin < end) {
+                // 从右边扫描中
+                if (pivot < numsArr[end]) {
+                    // 右边大于轴点元素，则直接移动end位置，无需改元素位置
+                    end--;
+                } else {
+                    numsArr[begin++] = numsArr[end];
+                    //一旦发生了交换，则改变扫描方向，当前内循环先退出
+                    break;
+                }
+            }
+            
+            while (begin < end) {
+                //从左边扫描中
+                if (pivot > numsArr[begin]) {
+                    // 左边小于轴点元素，则直接移动begin位置，无需改元素位置
+                    begin++;
+                } else {
+                    numsArr[end--] = numsArr[begin];
+                    //一旦发生了交换，则改变扫描方向，当前内循环先退出
+                    break;
+                }
+            }
+            
+        }
+        //最后还需要把轴点数据也更新到轴点位置（开始忘记了，坑自己）
+        numsArr[begin] = pivot;
+        return begin;
+    }
+};
+
 int main(int argc, const char * argv[]) {
     
     vector<int> nums = {10,9,8,7,5,1,1,2,0,0};
@@ -128,7 +190,10 @@ int main(int argc, const char * argv[]) {
 //    bubbleSort3(nums);
 //    selectionSort(nums);
     
-    Solution solu;
+//    MergeSort solu;
+//    nums = solu.sortArray(nums);
+    
+    QuickSort solu;
     nums = solu.sortArray(nums);
     
     for (int num : nums) {
