303f033cdcd753745708c9af4c6e386c3851edf2
qiujr <>
2021-03-29 13:10:04 +0800

    2. 两数相加

---
+++ 2. 两数相加/2. 两数相加/main.cpp
@@ -0,0 +1,83 @@
+//
+//  main.cpp
+//  2. 两数相加
+//
+//  Created by 邱俊荣 on 2021/3/29.
+//
+
+/*
+ 给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
+
+ 请你将两个数相加，并以相同形式返回一个表示和的链表。
+ 你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+ 
+ 示例 1：
+ 2 -> 4 -> 3
+ 5 -> 6 -> 4
+ ------------
+ 7 -> 0 -> 8
+ 输入：l1 = [2,4,3], l2 = [5,6,4]
+ 输出：[7,0,8]
+ 解释：342 + 465 = 807.
+ 
+ 示例 2：
+ 输入：l1 = [0], l2 = [0]
+ 输出：[0]
+ 
+ 示例 3：
+ 输入：l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+ 输出：[8,9,9,9,0,0,0,1]
+ 
+ 来源：力扣（LeetCode）
+ 链接：https://leetcode-cn.com/problems/add-two-numbers
+ 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+#include <iostream>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode *head = nullptr, *tail = nullptr;
+        int carry = 0;
+        while (l1 || l2) {
+            int n1 = l1 ? l1->val: 0;
+            int n2 = l2 ? l2->val: 0;
+            int sum = n1 + n2 + carry;
+            if (!head) {
+                head = tail = new ListNode(sum % 10);
+            } else {
+                tail->next = new ListNode(sum % 10);
+                tail = tail->next;
+            }
+            carry = sum / 10;
+            if (l1) {
+                l1 = l1->next;
+            }
+            if (l2) {
+                l2 = l2->next;
+            }
+        }
+        if (carry > 0) {
+            tail->next = new ListNode(carry);
+        }
+        return head;
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    
+    
+    
+    return 0;
+}
