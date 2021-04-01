73acb3a44194f3f5eb8c7c58d44fba344cc4b2e5
qiujr <>
2021-04-01 22:02:29 +0800

    拼接最大整数优化，加入最后交换位置标记

--- 0. 拼接第二大整数/0. 拼接第二大整数/main.cpp
+++ 0. 拼接第二大整数/0. 拼接第二大整数/main.cpp
@@ -20,11 +20,12 @@ using namespace std;
  */
 void makeMax(vector<int> &nums){
     
-    for (int begin = 0; begin < (int)nums.size(); begin++) {
-        
-        for (int end = (int)nums.size()-1; end > begin; end--) {
-            string a = to_string(nums[end]);
-            string b = to_string(nums[end-1]);
+    for (int end = (int)nums.size()-1; end > 0; end--) {
+        int lastMove = 0;//记录最后一次交换的位置，下次内循环从最后交换位置进行遍历
+        //冒泡法，每次内循环都把最小当前循环内最小的放到后面去了
+        for (int begin = 0; begin < end; begin++) {
+            string a = to_string(nums[begin]);
+            string b = to_string(nums[begin+1]);
             string ab = a+b;
             string ba = b+a;
             int ab_int, ba_int;
@@ -34,17 +35,19 @@ void makeMax(vector<int> &nums){
             stringstream sStream2;
             sStream2 << ba;
             sStream2 >> ba_int;
-            
-            if (ab_int > ba_int) {
-                
-                int tmp = nums[end];
-                nums[end] = nums[end-1];
-                nums[end-1] = tmp;
+            //ab组合小于ba组合，则说明a的数组的位置必然在b的后面
+            if (ab_int < ba_int) {
+                int tmp = nums[begin];
+                nums[begin] = nums[begin+1];
+                nums[begin+1] = tmp;
+                lastMove = begin+1;
             }
+            
         }
+        end = lastMove;
     }
     
-    //求第二大的数，则把前面第一和第二个元素换下位置
+    //求第二大的数，则把前面第一和第二个元素换下位置，屏蔽则是求的最大拼接整数
     int tmp = nums[0];
     nums[0] = nums[1];
     nums[1] = tmp;
